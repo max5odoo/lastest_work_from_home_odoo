c0fd0a0deb88e0a7925acaaebe3ce57658ee090c
‘Odoo <odoo@aktivsoftware.com>
2021-06-16 18:33:37 +0530

    [CHANGED]sale_order_update,product_catelog_line,sale_product_catalog.xml--->this changes is for defaul_search method

--- manthan/models/product_catelog_lines.py
+++ manthan/models/product_catelog_lines.py
@@ -1,10 +1,18 @@
-from odoo import fields, models
+from odoo import fields, models, api
+from odoo.osv import expression
 
 
-class Product_Catelog_Lines(models.Model):
+class ProductCatelogLines(models.Model):
     _name = 'product.catalog.lines'
     _description = 'this is product catelog lines'
 
     name = fields.Char()
     description = fields.Char()
     product_id = fields.Many2one('product.catalog')
+    is_active = fields.Boolean()
+
+    # @api.model
+    # def _name_search(self, name='', args=None, operator='ilike', limit=100, name_get_uid=None):
+    #     args = args or []
+    #     domain = [('description', operator, name)]
+    #     return self._search(expression.AND([domain, args]), limit=limit)
--- manthan/models/sale_order_update.py
+++ manthan/models/sale_order_update.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class Sale_Order_Up(models.Model):
@@ -7,7 +7,21 @@ class Sale_Order_Up(models.Model):
     is_ready = fields.Boolean()
     is_dt = fields.Boolean()
 
-    product_catalog_id = fields.Many2one('product.catalog')
+    product_catalogs_id = fields.Many2one('product.catalog')
+
+    @api.onchange("product_catalogs_id")
+    def product_cat_id(self):
+        for rec in self:
+            many2one_field = rec.product_catalogs_id.id
+            print(f'\n\n\n\n\n{many2one_field}\n\n\n\n')
+            rec.env['product.catalog.lines'].search([]).write({
+                'is_active': False
+            })
+            product_find = rec.env['product.catalog.lines'].search([('product_id', '=', many2one_field)]).write({
+                'is_active' : True
+            })
+            print(f'\n\n\n\n\nthis is product find--->{product_find}\n\n\n\n')
+
 
 
 class Sale_Order_Lines_Product(models.Model):
